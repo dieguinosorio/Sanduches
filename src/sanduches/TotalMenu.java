2ffb1e86a153d60a8d75936daa46fbb93d1f6005
dieguinosorio <dieguinosorio@gmail.com>
2017-10-02 10:49:08 -0500

    Update TotalMenu.java

--- src/sanduches/TotalMenu.java
+++ src/sanduches/TotalMenu.java
@@ -14,8 +14,13 @@ public class TotalMenu {
     public void Total(String TpPan, String TpCarne, String TpAcom, int EdadCli, String NmCliente) {
         double SubTotal = 0, Desc = 0, Total = 0;
         if (TpPan.equals("Pan Tradicional") && TpCarne.equals("Jamón") && TpAcom.equals("Queso")){
-            SubTotal = 11000.0;
-        } else {
+            SubTotal = 8550;
+            
+        } 
+        else if (TpPan.equals("Pan Tradicional") && TpCarne.equals("Pavo") && TpAcom.equals("Mayonesa")) {
+            precioSanduche = 11000;
+        } 
+        else {
             SubTotal = 10800.0;
         }
         if (EdadCli >= 65) {
